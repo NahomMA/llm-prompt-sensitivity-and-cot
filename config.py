43edf41842378b26b9284747ffd45445a03a60b1
NahomMA <nbirhanm@gmail.com>
2026-03-03 20:30:10 -0500

    first draft

---
+++ config.py
@@ -0,0 +1,8 @@
+config={
+        "id": "openai-gpt4",
+        "type": "openai",
+        "model": "gpt-4.1",
+        "api_key_env": "OPENAI_API_KEY",
+        "temperature": 0.0,
+        "max_tokens": 2000
+    }
---
+++ main.py
@@ -0,0 +1,4 @@
+def main():
+    print("Hello from hw2!")
+if __name__ == "__main__":
+    main()
